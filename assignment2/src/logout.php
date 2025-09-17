0992c03c2f631dde2f767c357d22427d35e529e8
Ada Ayman <adaayman12@gmail.com>
2025-09-17 19:13:36 -0400

    Delete assignment2 directory

--- assignment2/src/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-// src/logout.php
-session_start();
-session_unset();     // remove all session variables
-session_destroy();   // destroy the session itself
-header('Location: /assignment2/index.php');
-exit;
