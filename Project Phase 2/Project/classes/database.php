936a6a693566446748520c9631f53bba9bf623b2
Ada Ayman <adaayman12@gmail.com>
2025-12-02 19:07:56 -0500

    changed database.php it so it can work on georgian server as well.

--- Project Phase 2/Project/classes/database.php
+++ Project Phase 2/Project/classes/database.php
@@ -2,22 +2,25 @@
 
 class Database
 {
-    private string $host = '127.0.0.1';
-    private string $db   = 'rule_lawyers';
-    private string $user = 'root';
-    private string $pass = '';
-    private string $charset = 'utf8mb4';
-
     public function getConnection(): PDO
     {
-        $dsn = "mysql:host={$this->host};dbname={$this->db};charset={$this->charset}";
+       
+        if ($_SERVER['HTTP_HOST'] === 'localhost' || $_SERVER['HTTP_HOST'] === '127.0.0.1') {
+            // LOCAL (XAMPP)
+            $dsn  = 'mysql:host=127.0.0.1;dbname=rule_lawyers;charset=utf8mb4';
+            $user = 'root';
+            $pass = '';
+        } else {
+            // SERVER (Georgian)
+            $dsn  = '';
+            $user = '';
+            $pass = '';
+        }
 
-        $options = [
-            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES   => false,
-        ];
+        $pdo = new PDO($dsn, $user, $pass, [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        ]);
 
-        return new PDO($dsn, $this->user, $this->pass, $options);
+        return $pdo;
     }
 }
